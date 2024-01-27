8aa895fac9fe6707253f734db781de4fef474067
110020023 <dppss980777@gapp.nthu.edu.tw>
2024-01-27 11:00:23 +0800

    Update

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,13 @@
 <template>
-<<<<<<< Updated upstream
   <router-view />
-=======
-   <button v-if="$route.path !== '/login' && $route.path !== '/register'" @click="goToAboutPage">About Us</button>
-  <router-view></router-view>
->>>>>>> Stashed changes
 </template>
 
 <script>
 
 export default {
   name: 'App',
-<<<<<<< Updated upstream
   components: {
   }
-=======
-  methods: {
-    goToAboutPage() {
-      this.$router.push("/login");
-    },
-  },
->>>>>>> Stashed changes
 }
 </script>
 
